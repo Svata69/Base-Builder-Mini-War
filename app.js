95d0dd5d297466e21aba06b5f465c798c0a7b3e3
Svata69 <s.michal31.5@gmail.com>
2026-09-01 19:14:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -882,8 +882,9 @@ function getGridCoordinatesFromMouse(event) {
     const curW = isBoxPlacing ? boxWidth : getCurrentWidth();
     const curD = isBoxPlacing ? boxDepth : getCurrentDepth();
 
-    let snapX = Math.round((hitPoint.x - curW / 2) / gridStep) * gridStep + curW / 2;
-    let snapZ = Math.round((hitPoint.z - curD / 2) / gridStep) * gridStep + curD / 2;
+    // Čistý a stabilní snapping pro střed budovy
+    let snapX = Math.round(hitPoint.x / gridStep) * gridStep;
+    let snapZ = Math.round(hitPoint.z / gridStep) * gridStep;
 
     const maxX = (gridWidth / 2) - (curW / 2);
     const maxZ = (gridHeight / 2) - (curD / 2);
